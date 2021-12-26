5f60f46e9549092508b87e012fd2e529002db07d
KHUSHWANT PARIHAR <50989808+WOLFIEEEE@users.noreply.github.com>
2021-12-26 17:49:50 +0530

    Time: 112 ms (83.54%), Space: 63.3 MB (57.07%) - LeetHub

--- largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,76 +1,34 @@
 class Solution {
 public:
-    void print(vector<int> a)
-    {
-        for(auto it :a) cout << it << endl;
-    }
     int largestRectangleArea(vector<int>& h) {
         
-        int n = h.size();
-        vector<int> ls(n , 0);
-        vector<int> rs(n , 0);
-        
-        stack<int> right;
-        stack<int> st;
-        
-        for(int i = n-1 ; i >= 0 ; i--)
+        stack<int> index;
+        h.push_back(0);
+        int maxi = 0;
+        int size = h.size();
+        int area = 0;
+        for(int i = 0 ; i < size ; i ++)
         {
-            if(st.empty())
+            while(!index.empty() && h[index.top()] > h[i])
             {
-                st.push(i);
-                rs[i] = n;
-                continue;
+                int k = index.top();
+                index.pop();
+                int l;
+                if(index.empty())
+                {
+                    l = -1;
+                }else
+                {
+                    l = index.top();
+                }
+                
+                area = max(area , (i-l-1)*h[k]);  
             }
             
-            while(!st.empty() && h[st.top()] >= h[i])
-            {
-                st.pop();
-            }
-            
-            if(st.empty())
-            {
-                rs[i] = n;
-            }else
-            {
-                rs[i] = st.top();
-            }
-            
-            st.push(i);
+            index.push(i);
         }
         
-        
-        for(int i =  0; i < n ; i++)
-        {
-            if(right.empty())
-            {
-                right.push(i);
-                ls[i] = -1;
-                continue;
-            }
-            
-             while(!right.empty() && h[right.top()] >= h[i])
-            {
-                right.pop();
-            }
-            
-            if(right.empty())
-            {
-                ls[i] = -1;
-            }else
-            {
-                ls[i] = right.top();
-            }
-            
-            right.push(i);
-        }
-        
-        int maxi = INT_MIN;
-        
-        for(int i = 0 ; i < n ; i++)
-        {
-            maxi = max(maxi , h[i]*(rs[i]-ls[i]-1));
-        }
-        return maxi;
+        return area;
         
     }
 };
