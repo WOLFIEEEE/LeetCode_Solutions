8d566b17073589165277352d34525e87bb7a3e14
KHUSHWANT PARIHAR <50989808+WOLFIEEEE@users.noreply.github.com>
2021-12-30 01:20:23 +0530

    Time: 13 ms (62.77%), Space: 7.7 MB (17.81%) - LeetHub

--- implement-strstr/implement-strstr.cpp
+++ implement-strstr/implement-strstr.cpp
@@ -1,28 +1,80 @@
 class Solution {
 public:
-    int strStr(string h, string needle) {
+    
+    vector<int> lps(string needle)
+    {
+        vector<int> ans;
         
-        int l = needle.length();
+        int n =  needle.length();
         
-        if(h == needle)
+        
+        ans.push_back(0);
+        int len = 0;
+        int j = 1;
+        while(j < n)
         {
-            return 0;
-        }
-        
-        
-        if(h.length()==0 || h.length() < l)
-        {
-            return -1;
-        }
-        
-        for (int i=0 ; i< h.length()-l+1 ; i++ )
-        {
-            if(h.substr(i , l) == needle)
+            if(needle[len] == needle[j])
             {
-                return i;
+                ans.push_back(len+1);
+                len++;
+                j++;
+            }else
+            {
+                if(len != 0)
+                {
+                    len = ans[len-1];
+                }else
+                {
+                    ans.push_back(0);
+                    j++;
+                }
             }
         }
         
+        return ans;
+    }
+    int strStr(string h, string needle) {
+        
+        
+        vector<int> lp = lps(needle);
+        
+        int ch = needle.length();
+        
+        int  i = 0;
+        int j = 0;
+        int len = h.length();
+        
+        if(ch == 0 && len == 0) return 0;
+        if(len == 0 || len < ch) return -1;
+        if(h == needle || ch == 0) return 0;
+        
+        while(i < len)
+        {
+            
+           
+            if(h[i] == needle[j])
+            {
+                i++;
+                j++;
+            }else
+            {
+                if(j != 0)
+                {
+                    j = lp[j-1];
+                }else
+                {
+                    i++;
+                }
+            }
+            
+            
+             if(j == ch)
+            {
+                return i-j;
+            }
+            
+            
+        }
         return -1;
         
     }
