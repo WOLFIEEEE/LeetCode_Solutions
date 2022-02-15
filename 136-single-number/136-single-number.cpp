d1f46f48e4f5e1f73ced86333fc02767711698db
KHUSHWANT PARIHAR <50989808+WOLFIEEEE@users.noreply.github.com>
2022-02-15 23:40:38 +0530

    Time: 22 ms (65.83%), Space: 16.8 MB (85.89%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        
+        int a = 0;
+        
+        for(auto it : nums)
+        {
+            a = a^it;
+        }
+        
+        return a;
+        
+    }
+};
