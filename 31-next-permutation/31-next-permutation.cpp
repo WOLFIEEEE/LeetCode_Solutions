766cb6bb9574339ff36e0fe4743f6564ab0d13bf
KHUSHWANT PARIHAR <50989808+WOLFIEEEE@users.noreply.github.com>
2022-09-05 06:17:57 -0700

    Time: 8 ms (45.96%), Space: 12 MB (74.43%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -2,7 +2,35 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         
-        next_permutation(nums.begin() , nums.end()); 
+        int index1 , index2;
+        
+        for(index1 = nums.size()-2 ; index1 >= 0 ; index1--)
+        {
+            if(nums[index1] < nums[index1+1])
+            {
+                break;
+            }
+        }
+        
+        if(index1 < 0) reverse(nums.begin() , nums.end());
+        else
+        {
+            for(index2 = nums.size()-1 ; index2 > index1 ; index2--)
+            {
+                if(nums[index1] < nums[index2] )
+                {
+                    break;
+                }
+            }
+            
+            swap(nums[index1] , nums[index2]);
+            reverse(nums.begin()+index1+1 , nums.end());
+        }
+        
+        
+        
+        // reverse(nums.begin()+index1+1 , nums.end());
+        
         
     }
 };
