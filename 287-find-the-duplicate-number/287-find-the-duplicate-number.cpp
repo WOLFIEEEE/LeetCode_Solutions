2f0f3918dce499359b5b95cb07e926354b4aac94
KHUSHWANT PARIHAR <50989808+WOLFIEEEE@users.noreply.github.com>
2022-09-06 02:43:41 -0700

    Time: 94 ms (99.80%), Space: 61.3 MB (61.80%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -4,18 +4,17 @@ public:
         
         int n = nums.size()+1;
         
-        vector<int> check(n , 0);
-        
-        for(int i = 0 ; i < nums.size() ; i++)
+        for(auto it:nums)
         {
-            check[nums[i]-1]++;
+            int index = abs(it)-1;
+            
+            nums[index] *= -1;
+            
+            if(nums[index] > 0)
+            {
+                return abs(index)+1;
+            }
         }
-        
-        for(int i = 0 ; i < n-1 ; i++)
-        {
-            if(check[i] > 1) return i+1;
-        }
-        
         return 0;
         
     }
