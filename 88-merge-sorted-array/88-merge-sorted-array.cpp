558b79e3f2658bdbe83b57ab07a8f1a01e560c53
KHUSHWANT PARIHAR <50989808+WOLFIEEEE@users.noreply.github.com>
2022-09-06 02:28:59 -0700

    Time: 7 ms (29.07%), Space: 9.2 MB (30.14%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -3,15 +3,20 @@ public:
 	void merge(vector<int>& arr1, int m, vector<int>& arr2, int n) {
 		if(n == 0) return;
 		int i = 0;
-		while(i < m){
-			if(arr1[i] <= arr2[0]){
-				i++;
-			}else{
-				swap(arr1[i],arr2[0]);
-				sort(arr2.begin(),arr2.end());
-			}
-		}
-		i = m;
-		for(int j : arr2) arr1[i++] = j;
+		
+        while(i < m)
+        {
+            if(arr1[i] <= arr2[0])
+            {
+                i++;
+            }else
+            {
+                swap(arr1[i] , arr2[0]);
+                sort(arr2.begin() , arr2.end());
+            }
+        }
+        
+       i = m;
+        for(auto it:arr2) arr1[i++] = it;
 	}
 };
